09c765a8b81605acf2acd28a84a8f0a2ab9558ef
Erik <kiremate@hotmail.com>
2022-01-07 23:18:01 +0100

    Created class Stack

---
+++ source/Stack.h
@@ -0,0 +1,35 @@
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ *                                                                             *
+ *  Stack                                                                      *
+ *  Copyright © 2022+ Erik Mateos Morcillo                                     *
+ *                                                                             *
+ *  Distributed under the MIT License                                          *
+ *  See documents/LICENSE.TXT												   *
+ *                                                                             *
+ *  bloodmaskofficial@gmail.com                                                *
+ *                                                                             *
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef STACK_H
+#define STACK_H
+
+namespace bloodmask {
+
+
+
+	class Stack {
+
+	private:
+
+
+	public:
+
+
+	};
+
+
+
+}
+
+#endif // !STACK
